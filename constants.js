367636297afa1c87b5c8b8a93fea8c966db7f1bf
mykyta <mykytazviahin@yahoo.com>
2021-11-17 15:43:28 +0200

    fix: fix to redirect from initial page

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
-export const INITIAL_URL =
-  "https://www.olx.ua/d/obyavlenie/iphone-5-white-32gb-IDN4Oiy.html?sd=1#44b44512b4";
+export const INITIAL_URL = "https://www.olx.ua";
 
 export const SELECTOR_TO_REDIRECT =
   'section[data-cy="ad-recommendations"] > div > div > div > div > div > a';
@@ -7,3 +6,8 @@ export const SELECTOR_TO_REDIRECT =
 export const TITLE_SELECTOR = 'h1[data-cy="ad_title"]';
 export const PRICE_SELECTOR = 'div[data-testid="ad-price-container"]';
 export const DESCRIPTION_SELECTOR = 'div[data-cy="ad_description"]';
+
+export const INPUT_SELECTOR='input[id="headerSearch"]';
+export const BUTTON_SELECTOR='input[id="submit-searchmain"]';
+
+export const KEY_WORD = 'iphone';
--- index.js
+++ index.js
@@ -7,13 +7,21 @@ import { stringFormatter } from "./utils.js";
 (async () => {
   let olxData = [];
   await browser.url(constants.INITIAL_URL);
+  await browser.$(constants.INPUT_SELECTOR).setValue(constants.KEY_WORD);
+  await browser.$(constants.BUTTON_SELECTOR).click();
+  //FIX TO REDIRECT FROM SEARCH SCREEN
+  const initialRedirect = await browser
+    .$('a[data-cy="listing-ad-title"]')
+    .getAttribute("href");
+  //REMOVE THIS REDIRECT LATER
+  await browser.url("https://www.olx.ua/d/obyavlenie/prodam-iphone-8-IDN3pl5.html?sd=1#573a179f52;promoted");
   for (let index = 0; index < 100; index++) {
     const redirectRef = await browser
       .$(constants.SELECTOR_TO_REDIRECT)
       .getAttribute("href");
 
     if (index !== 0) {
-      await browser.url("https://www.olx.ua" + redirectRef);
+      await browser.url(constants.INITIAL_URL + redirectRef);
     }
 
     const title = await browser.$(constants.TITLE_SELECTOR).getText();
